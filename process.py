b22e70d089d146f5f1a3f9d910fc8102f3833350
Saul Shanabrook <s.shanabrook@gmail.com>
2024-01-28 18:18:14 -0500

    split lists by commas

--- process.py
+++ process.py
@@ -6,6 +6,7 @@ from rich.progress import track
 ID_PREFIX = "MainContent_FindBHProvider1_DataList1_lbl"
 ID_SUFFIX = "_0"
 
+SPLIT_COLUMNS = ["SpecialInterests", "AgeCategories", "BoardCertification"]
 
 with open("./data.jsonl", "r") as json_file:
     with open("processed.jsonl", "w") as outfile:
@@ -13,7 +14,11 @@ with open("./data.jsonl", "r") as json_file:
             root = BeautifulSoup(json.loads(line))
             for provider in root.find_all("p"):
                 data: dict[str, str] = {
-                    attr.attrs["id"][len(ID_PREFIX) :].split("_")[0]: str(attr.string)
+                    (NAME := attr.attrs["id"][len(ID_PREFIX) :].split("_")[0]): str(
+                        attr.string
+                    )
+                    if NAME not in SPLIT_COLUMNS
+                    else str(attr.string).split(", ")
                     for attr in provider.find_all(
                         "span", id=lambda x: x and x.startswith(ID_PREFIX)
                     )
